0a174a17095a949ba481f6de743adcebea1f3784
Brandon Bennett <bbennett@subspace.com>
2020-09-21 11:45:51 -0600

    zsh: fix pmton function when precmd_functions is not defined

--- roles/zsh/files/zshrc.d/40prompt.zsh
+++ roles/zsh/files/zshrc.d/40prompt.zsh
@@ -4,7 +4,11 @@ function powerline_precmd() {
 }
 
 function pmton() {
-  [[ ${precmd_functions[(ie)powerline_precmd]} -le ${#precmd_functions} ]] && return 
+  for s in "${precmd_functions[@]}"; do
+    if [ "$s" = "powerline_precmd" ]; then
+      return
+    fi
+  done
   precmd_functions+=(powerline_precmd)
 }
 
