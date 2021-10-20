647c81574563ab67f5c9ec2d2a159454cd2011ec
Brandon Bennett <brandon@brbe.me>
2021-10-20 14:56:17 -0600

    prepend go paths

--- roles/zsh/files/zshrc.d/60go.zsh
+++ roles/zsh/files/zshrc.d/60go.zsh
@@ -1,9 +1,9 @@
 if [ -d /opt/go ]; then
 	export GOROOT=/opt/go
-	append_path /opt/go/bin
+	prepend_path /opt/go/bin
 fi
 
 [ ! -x $(whiff go) ] && return
 
 export GOPATH=$HOME/.go
-append_path $GOPATH/bin
+prepend_path $GOPATH/bin
