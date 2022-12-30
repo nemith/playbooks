93f9bd39cd58aff5098b627ec24405c68d424849
Brandon Bennett <brandon@brbe.me>
2022-12-30 14:32:15 -0700

    workstation: lint files and setup for running and any user

--- workstation/roles/zsh/files/zshrc.d/40prompt.zsh
+++ workstation/roles/zsh/files/zshrc.d/40prompt.zsh
@@ -1,22 +1,24 @@
-function powerline_precmd() {
-    PLGO="$(which powerline-go)"
-    PS1="$($PLGO -error $? -shell zsh)"
-}
+[[ $TERM_PROGRAM == "Apple_Terminal" || $TERM == "linux" ]] && return
 
-function pmton() {
+
+if [ -x $(whiff oh-my-posh) ]; then
+  eval "$(oh-my-posh init zsh --config $HOME/.config/omp/config.toml)"
+  return
+fi
+
+if [ -x $(whiff powerline-go) ]; then
+  function powerline_precmd() {
+      PLGO="$(which powerline-go)"
+      PS1="$($PLGO -error $? -shell zsh)"
+  }
+
   for s in "${precmd_functions[@]}"; do
     if [ "$s" = "powerline_precmd" ]; then
       return
     fi
   done
   precmd_functions+=(powerline_precmd)
-}
-
-function pmtoff() {
-  precmd_functions=("${(@)precmd_functions:#powerline_precmd}")
-  PS1="%# "
-}
-
-if [ "$TERM" != "linux" ]; then
-    pmton
+  return
 fi
+
+echo "please install oh-my-posh or powerline-go"
